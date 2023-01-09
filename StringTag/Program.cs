61fbcc732e88c1d6f12534d4ed0574a2a974292e
anh.nguyen49 <anh.nguyen49@ntq-solution.com.vn>
2023-01-09 10:09:56 +0700

    Add project files.

---
+++ StringTag/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace StringTag
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string fileName = @"F:\tempdata\Jsons\WordImportColorSupportCheckRules.json";
+           
+            var Result = new List<string>();
+            var text = File.ReadLines(fileName);
+            foreach (var line in text)
+            {
+                var regex = new Regex(@"(?<= "")(.*)(?="")");
+                string res = regex.Match(line).ToString();
+                if (!Result.Contains(res))
+                {
+                    Result.Add(res);
+                }
+            }
+
+            foreach (var temp in Result)
+            {
+                Console.WriteLine(temp);
+            }
+            Console.ReadKey();
+        }
+    }
+}
